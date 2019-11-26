c0c3fca2c2f8b951982fd915e89f36f5814893e4
clhuang224 <clhuang224@gmail.com>
2019-11-26 15:18:22 +0800

    Fix bugs

--- hw3/ImageEncrypt.py
+++ hw3/ImageEncrypt.py
@@ -12,36 +12,36 @@ def main():
     if(len(sys.argv) == 4):
         file_path = sys.argv[1]
         mode = sys.argv[2]
-        key = sys.argv[3]
+        key = (sys.argv[3]).upper()
     else:
         file_path = input('Enter the path of the image (Ex. penguin.png): ')
         mode = input(
-            'Choose the mode of block cypher (ECB:0 CBC:1 CTR:2): ')
-        key = input('Enter the key (not case-sensitive): ')
+            'Enter the mode of block cipher (ECB/ CBC/ CTR): ')
+        key = (input('Enter the key (16 characters, not case-sensitive): ')).upper()
 
     # 取出檔案名稱和副檔名
     filename, ext = os.path.splitext(os.path.split(file_path)[1])
 
     # 把檔案轉存成 ./____.ppm
-    Image.open(file_path).save(filename+'.ppm')
+    Image.open(file_path).save(filename+'_'+mode+'.ppm')
 
     # 對 .ppm 加密
     if(mode == 'ECB'):
-        ECB(filename + '.ppm', key)
+        ECB(filename + '_'+mode + '.ppm', key)
     elif(mode == 'CBC'):
-        CBC(filename + '.ppm', key)
+        CBC(filename + '_'+mode + '.ppm', key)
     elif(mode == 'CTR'):
-        CTR(filename + '.ppm', key)
+        CTR(filename + '_'+mode + '.ppm', key)
 
     # 將 .ppm 轉存成 ./____.xxx (jpeg/gif/png/bmp)
     target_format = ext[1:]
     if(target_format.lower() == 'jpg'):
         target_format = 'jpeg'
-    Image.open(filename + '.ppm').save(filename +
-                                       '_encrypted' + ext, target_format)
+    Image.open(filename + '_'+mode + '.ppm').save(filename + '_' + mode +
+                                                  '_encrypted' + ext, target_format)
 
     # 刪除 ./____.ppm
-    os.remove(filename + '.ppm')
+    os.remove(filename + '_'+mode + '.ppm')
 # // end main()
 
 
@@ -63,11 +63,12 @@ def ECB(file_path, key):
             result += ciphertext
             current_block = file.read(16)
         # PKCS padding
-        padding_amount = 16 - len(current_block)
-        padding = (padding_amount*16).to_bytes(1, 'big')
-        current_block += padding * padding_amount
-        ciphertext = cipher.encrypt(current_block)
-        result += ciphertext
+        if(len(current_block) != 0):
+            padding_amount = 16 - len(current_block)
+            padding = (padding_amount*16).to_bytes(1, 'big')
+            current_block += padding * padding_amount
+            ciphertext = cipher.encrypt(current_block)
+            result += ciphertext
     with open(file_path, 'wb') as file:
         file.write(result)
 # // end ECB()
@@ -102,12 +103,14 @@ def CBC(file_path, key):
             result += ciphertext
             current_block = file.read(16)
         # PKCS padding
-        padding_amount = 16 - len(current_block)
-        padding = (padding_amount*16).to_bytes(1, 'big')
-        current_block += padding * padding_amount
-        current_block = bytes([current_block[i] ^ i_v[i] for i in range(16)])
-        ciphertext = cipher.encrypt(current_block)
-        result += ciphertext
+        if(len(current_block) != 0):
+            padding_amount = 16 - len(current_block)
+            padding = (padding_amount*16).to_bytes(1, 'big')
+            current_block += padding * padding_amount
+            current_block = bytes([current_block[i] ^ i_v[i]
+                                   for i in range(16)])
+            ciphertext = cipher.encrypt(current_block)
+            result += ciphertext
     with open(file_path, 'wb') as file:
         file.write(result)
 # // end CBC()
@@ -143,12 +146,13 @@ def CTR(file_path, key):
             # IV += 1
             i_v = (int.from_bytes(i_v, 'big') + 1).to_bytes(16, 'big')
         # PKCS padding
-        padding_amount = 16 - len(current_block)
-        padding = (padding_amount*16).to_bytes(1, 'big')
-        current_block += padding * padding_amount
-        ciphertext_i_v = cipher.encrypt(i_v)
-        ciphertext = bytes([current_block[i] ^ ciphertext_i_v[i]
-                            for i in range(16)])
+        if(len(current_block) != 0):
+            padding_amount = 16 - len(current_block)
+            padding = (padding_amount*16).to_bytes(1, 'big')
+            current_block += padding * padding_amount
+            ciphertext_i_v = cipher.encrypt(i_v)
+            ciphertext = bytes([current_block[i] ^ ciphertext_i_v[i]
+                                for i in range(16)])
         result += ciphertext
     with open(file_path, 'wb') as file:
         file.write(result)
