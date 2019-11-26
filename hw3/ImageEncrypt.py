5e014065c0af3f7f40fd995f0bbb5b2f899b8234
clhuang224 <clhuang224@gmail.com>
2019-11-26 13:51:30 +0800

    Finish ImageEncrypt

--- hw3/ImageEncrypt.py
+++ hw3/ImageEncrypt.py
@@ -6,92 +6,154 @@ from Crypto.Cipher import AES
 
 def main():
     # 輸入圖檔路徑、 block cyphor mode 和 key
-    filePath = None
+    file_path = None
     mode = None
     key = None
     if(len(sys.argv) == 4):
-        filePath = sys.argv[1]
+        file_path = sys.argv[1]
         mode = sys.argv[2]
         key = sys.argv[3]
     else:
-        filePath = input('Enter the path of the image (Ex. penguin.png): ')
+        file_path = input('Enter the path of the image (Ex. penguin.png): ')
         mode = input(
-            'Choose the mode of block cypher (ECB:0 CBC:1 Self-made:2): ')
+            'Choose the mode of block cypher (ECB:0 CBC:1 CTR:2): ')
         key = input('Enter the key (not case-sensitive): ')
 
     # 取出檔案名稱和副檔名
-    filename, ext = os.path.splitext(os.path.split(filePath)[1])
+    filename, ext = os.path.splitext(os.path.split(file_path)[1])
 
     # 把檔案轉存成 ./____.ppm
-    Image.open(filePath).save(filename+'.ppm')
+    Image.open(file_path).save(filename+'.ppm')
 
     # 對 .ppm 加密
-    if(mode == '0'):
+    if(mode == 'ECB'):
         ECB(filename + '.ppm', key)
-    elif(mode == '1'):
+    elif(mode == 'CBC'):
         CBC(filename + '.ppm', key)
-    elif(mode == '2'):
-        selfMade(filename + '.ppm', key)
+    elif(mode == 'CTR'):
+        CTR(filename + '.ppm', key)
 
     # 將 .ppm 轉存成 ./____.xxx (jpeg/gif/png/bmp)
-    targetFormat = ext[1:]
-    if(targetFormat.lower() == 'jpg'):
-        targetFormat = 'jpeg'
+    target_format = ext[1:]
+    if(target_format.lower() == 'jpg'):
+        target_format = 'jpeg'
     Image.open(filename + '.ppm').save(filename +
-                                       '_encrypted' + ext, targetFormat)
+                                       '_encrypted' + ext, target_format)
 
     # 刪除 ./____.ppm
-    # os.remove(filename + '.ppm')
+    os.remove(filename + '.ppm')
 # // end main()
 
-# 加解密用套件的AES
-# 除了第三個0A（換行符號）前的東西不變　其他的東西用16byte為單位丟進去做就對了
-# padding 的方式用 PKCS ？ (有N個 就寫N0補到滿)
 
-
-def ECB(filePath, key):
+def ECB(file_path, key):
     result = b''
-    with open(filePath, 'rb') as file:
+    with open(file_path, 'rb') as file:
         # 照抄前三行
-        enterAmount = 0
-        while(enterAmount != 3):
+        enter_amount = 0
+        while(enter_amount != 3):
             temp = file.read(1)
             result += temp
             if(temp == b'\n'):
-                enterAmount += 1
-        # 每個block加密
+                enter_amount += 1
+        # 每個 block 用 AES 加密
         cipher = AES.new(bytes(key.encode('utf-8')), AES.MODE_ECB)
-        currentBlock = file.read(16)
-        while(len(currentBlock) == 16):
-            ciphertext = cipher.encrypt(currentBlock)
-            # print(ciphertext)0
+        current_block = file.read(16)
+        while(len(current_block) == 16):
+            ciphertext = cipher.encrypt(current_block)
             result += ciphertext
-            currentBlock = file.read(16)
-        paddingAmount = 16 - len(currentBlock)
-        padding = (paddingAmount*16).to_bytes(1,'big')
-        currentBlock += padding * paddingAmount
-        ciphertext = cipher.encrypt(currentBlock)
+            current_block = file.read(16)
+        # PKCS padding
+        padding_amount = 16 - len(current_block)
+        padding = (padding_amount*16).to_bytes(1, 'big')
+        current_block += padding * padding_amount
+        ciphertext = cipher.encrypt(current_block)
         result += ciphertext
-    with open(filePath, 'wb') as file:
+    with open(file_path, 'wb') as file:
         file.write(result)
 # // end ECB()
 
 
-def CBC(filePath, key):
-    return
-# // end CBC()
-
-
-def selfMade(filePath, key):
-    i_v = ""
+def CBC(file_path, key):
+    # 產生 IV
+    i_v = ''
     i_p = [7, 15, 6, 1, 10, 8, 3, 11, 16, 13, 2, 9, 14, 4, 5, 12]
     i_p = i_p[(int(key[0]) % 16):] + i_p[:(int(key[0]) % 16)]
     for index in i_p:
         i_v += key[index - 1]
-    
+    i_v = bytes(i_v.encode('utf-8'))
+    result = b''
+    with open(file_path, 'rb') as file:
+        # 照抄前三行
+        enter_amount = 0
+        while(enter_amount != 3):
+            temp = file.read(1)
+            result += temp
+            if(temp == b'\n'):
+                enter_amount += 1
+        # 每個 block 用 AES 加密
+        cipher = AES.new(bytes(key.encode('utf-8')), AES.MODE_ECB)
+        current_block = file.read(16)
+        while(len(current_block) == 16):
+            current_block = bytes([current_block[i] ^ i_v[i]
+                                   for i in range(16)])
+            ciphertext = cipher.encrypt(current_block)
+            # 以密文作為下一個 IV
+            i_v = ciphertext
+            result += ciphertext
+            current_block = file.read(16)
+        # PKCS padding
+        padding_amount = 16 - len(current_block)
+        padding = (padding_amount*16).to_bytes(1, 'big')
+        current_block += padding * padding_amount
+        current_block = bytes([current_block[i] ^ i_v[i] for i in range(16)])
+        ciphertext = cipher.encrypt(current_block)
+        result += ciphertext
+    with open(file_path, 'wb') as file:
+        file.write(result)
+# // end CBC()
 
-    return
-# // end selfMade()
+
+def CTR(file_path, key):
+    # 產生 IV
+    i_v = ''
+    i_p = [7, 15, 6, 1, 10, 8, 3, 11, 16, 13, 2, 9, 14, 4, 5, 12]
+    i_p = i_p[(int(key[0]) % 16):] + i_p[:(int(key[0]) % 16)]
+    for index in i_p:
+        i_v += key[index - 1]
+    i_v = bytes(i_v.encode('utf-8'))
+
+    result = b''
+    with open(file_path, 'rb') as file:
+        # 照抄前三行
+        enter_amount = 0
+        while(enter_amount != 3):
+            temp = file.read(1)
+            result += temp
+            if(temp == b'\n'):
+                enter_amount += 1
+        # 每個 block 用 AES 加密
+        cipher = AES.new(bytes(key.encode('utf-8')), AES.MODE_ECB)
+        current_block = file.read(16)
+        while(len(current_block) == 16):
+            ciphertext_i_v = cipher.encrypt(i_v)
+            ciphertext = bytes(
+                [current_block[i] ^ ciphertext_i_v[i] for i in range(16)])
+            result += ciphertext
+            current_block = file.read(16)
+            # IV += 1
+            i_v = (int.from_bytes(i_v, 'big') + 1).to_bytes(16, 'big')
+        # PKCS padding
+        padding_amount = 16 - len(current_block)
+        padding = (padding_amount*16).to_bytes(1, 'big')
+        current_block += padding * padding_amount
+        ciphertext_i_v = cipher.encrypt(i_v)
+        ciphertext = bytes([current_block[i] ^ ciphertext_i_v[i]
+                            for i in range(16)])
+        result += ciphertext
+    with open(file_path, 'wb') as file:
+        file.write(result)
+# // end CTR()
 
 
-main()
+if __name__ == '__main__':
+    main()
