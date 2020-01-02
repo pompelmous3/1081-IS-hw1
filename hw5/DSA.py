f40e5385550747cc6e6ffd3636d4a8f2b81c2583
pompelmous3 <48688869+pompelmous3@users.noreply.github.com>
2020-01-02 20:43:01 +0800

    generate_key

--- hw5/DSA.py
+++ hw5/DSA.py
@@ -18,7 +18,23 @@ def main():
 
 
 def generate_key(key_length):
+	q = generateLargePrime(160)     # generate q first
+	print(len(bin(q)))
+	
+	p = q * generateLargePrime(865) + 1     # generate p
+	print(p.bit_length())
 
+	h = random.randrange(2, p - 1)      # random choose h
+	alpha = square_and_multiply(h, (p - 1) // q, p)
+
+	while square_and_multiply(alpha, q, p) != 1:
+        	h = random.randrange(2, p - 1)  # random choose h
+        	alpha = square_and_multiply(h, (p - 1) // q, p)
+
+	d = random.randrange(1, q)
+	beta = square_and_multiply(alpha, d, p)
+
+    	return p, q, alpha, beta, d
 # end generate_key()
 
 def sign(message):
